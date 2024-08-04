ebb379e9592287b84e38a0931fedcd47aee93943
karniib <hadikarnib03@gmail.com>
2024-08-04 17:43:31 +0300

    added login.jsx

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
+import { Link } from "react-router-dom";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-container">
+      <h1 className="login-header">Login</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="login-input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="login-input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button className="login-button" type="submit">
+          Login
+        </button>
+        <p>
+          Don't have an account?{" "}
+          <Link to="/register" className="link">
+            Register Here
+          </Link>
+        </p>
+      </form>
     </div>
   );
 };
