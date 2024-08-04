ba21f9f03ca3c7cf4a2c19716b4d9af519f818bc
karniib <hadikarnib03@gmail.com>
2024-08-04 17:43:55 +0300

    added register.jsx

--- frontend/src/pages/register/register.jsx
+++ frontend/src/pages/register/register.jsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "./register.css";
+
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle registration logic
+    console.log("Email:", email);
+    console.log("Username:", username);
+    console.log("Password:", password);
+    console.log("Phone Number:", phoneNumber);
+  };
+
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="register-container">
+      <h1 className="register-header">Register</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="register-input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="register-input-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className="register-input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div className="register-input-group">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input
+            type="text"
+            id="phoneNumber"
+            name="phoneNumber"
+            value={phoneNumber}
+            placeholder="enter your number eg:03123123"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            required
+          />
+        </div>
+        <button className="register-button" type="submit">
+          Register
+        </button>
+      </form>
     </div>
   );
 };
