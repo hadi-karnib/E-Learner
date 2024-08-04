5ef3e509400c7ad224d629732dede6c99655b1c5
karniib <hadikarnib03@gmail.com>
2024-08-04 13:45:30 +0300

    created class schema

---
+++ Backend/models/Class.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const classSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    instructor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Class", classSchema);
